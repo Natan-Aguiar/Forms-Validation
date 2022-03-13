4f1d07b90bec55548115ba0f281120dd0bbb5193
Natan-Aguiar <99612730+Natan-Aguiar@users.noreply.github.com>
2022-03-13 11:21:36 -0300

    formulando

--- form.js
+++ form.js
@@ -1,13 +1,13 @@
 const register = document.querySelector(".creat-account");
 
 register.onclick = () => {
-document.querySelector(".form-sign-in").style.display = "initial";
+document.querySelector(".form-sign-up").style.display = "initial";
 document.querySelector(".form-login").style.display = "none";
 }
 
 const login = document.querySelector(".btn-login");
 
 login.onclick = () => {
-document.querySelector(".form-sign-in").style.display = "none";
+document.querySelector(".form-sign-up").style.display = "none";
 document.querySelector(".form-login").style.display = "initial";
 }
